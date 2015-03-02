6251a9e8baf723324122b59e9694e48c1c3d4026
Nathaniel Davis <nwdavis88@gmail.com>
2015-03-02 16:23:14 -0600

    Testing MongoDb connectivity

---
+++ 360Feedback/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _360Feedback.Models
+{
+    public class Category
+    {
+        public string name { get; set; }
+        public List<string> values { get; set; }
+    }
+}
---
+++ 360Feedback/Models/Question.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _360Feedback.Models
+{
+    public class Question
+    {
+        public string title { get; set; }
+        public List<Category> categories { get; set; }
+    }
+}
