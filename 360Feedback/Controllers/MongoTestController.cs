4011206fba51f66b9a7e4cefcdd955233bc72d6a
Nathaniel Davis <nwdavis88@gmail.com>
2015-03-23 16:44:24 -0500

    Changed access to Mongolab to using REST API. Please ensure that you install the Microsoft.AspNet.WebApi.Client package if not installed when you merge this version.

--- 360Feedback/Controllers/MongoTestController.cs
+++ 360Feedback/Controllers/MongoTestController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using MongoDB.Bson;
@@ -13,42 +16,56 @@ namespace _360Feedback.Controllers
     public class MongoTestController : Controller
     {
         // GET: MongoTest
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            string uri = "mongodb://admin:admin@ds049211.mongolab.com:49211/wctc-360-feedback";
+            //string uri = "mongodb://admin:admin@ds049211.mongolab.com:49211/wctc-360-feedback";
             
-            MongoUrl mongoUrl = new MongoUrl(uri);
-            MongoClient client = new MongoClient(mongoUrl);
-            MongoServer server = client.GetServer();
-            MongoDatabase database = server.GetDatabase(mongoUrl.DatabaseName);
+            //MongoUrl mongoUrl = new MongoUrl(uri);
+            //MongoClient client = new MongoClient(mongoUrl);
+            //TimeSpan span = client.Settings.ConnectTimeout;
+            //MongoServer server = client.GetServer();
+            //MongoDatabase database = server.GetDatabase(mongoUrl.DatabaseName);
 
-            var db = database.GetCollection<BsonDocument>("questions").FindAll();
+            //var db = database.GetCollection<BsonDocument>("questions").FindAll();
 
-            var questions = new List<Question>();
+            string url = "https://api.mongolab.com/api/1/databases/wctc-360-feedback/";
+            string key = "oHIeU1xIFPhPKDrXzkO90gQTWe7wyswW";
 
-            foreach(var document in db)
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("collections/questions?apiKey=" + key);
+            if(response.IsSuccessStatusCode)
             {
-                Question question = new Question();
-                List<Category> categories = new List<Category>();
-                question.title = document.GetElement("title").ToString();
-                foreach(var category in document.GetElement("categories").Value.AsBsonArray)
-                {
-                    Category addCategory = new Category();
-                    var categoryDoc = category.ToBsonDocument();
-                    addCategory.name = categoryDoc.GetElement("name").ToString();
-                    List<string> values = new List<string>();
-                    foreach(var value in categoryDoc.GetElement("values").Value.AsBsonArray)
-                    {
-                        values.Add(value.ToString());
-                    }
-                    addCategory.values = values;
-                    categories.Add(addCategory);
-                }
-                question.categories = categories;
-                questions.Add(question);
-            }
+                List<Question> questions = await HttpContentExtensions.ReadAsAsync<List<Question>>(response.Content);
 
-            return View(questions);
+                //foreach(var document in db)
+                //{
+                //    Question question = new Question();
+                //    List<Category> categories = new List<Category>();
+                //    question.title = document.GetElement("title").ToString();
+                //    foreach(var category in document.GetElement("categories").Value.AsBsonArray)
+                //    {
+                //        Category addCategory = new Category();
+                //        var categoryDoc = category.ToBsonDocument();
+                //        addCategory.name = categoryDoc.GetElement("name").ToString();
+                //        List<string> values = new List<string>();
+                //        foreach(var value in categoryDoc.GetElement("values").Value.AsBsonArray)
+                //        {
+                //            values.Add(value.ToString());
+                //        }
+                //        addCategory.values = values;
+                //        categories.Add(addCategory);
+                //    }
+                //    question.categories = categories;
+                //    questions.Add(question);
+                //}
+                questions.Reverse();
+                return View(questions);
+            }
+            return View();
         }
     }
 }
