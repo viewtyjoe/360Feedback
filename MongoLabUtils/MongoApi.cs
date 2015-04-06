74af319868ee4c2c81bb10fdc28cfa2785d0a5cc
Nathaniel Davis <nwdavis88@gmail.com>
2015-04-06 17:17:19 -0500

    Added utility class to handle REST calls to MongoLab. Get all functions, Insert does not currently function, returns 400 Bad Request

---
+++ MongoLabUtils/MongoApi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace MongoLabUtils
+{
+    public class MongoApi
+    {
+        public string ApiKey { get; set; }
+        public string DbUser { get; set; }
+        public string DbPassword { get; set; }
+        public string Database { get; set; }
+        private readonly string baseUrl = "https://api.mongolab.com/api/1/databases/";
+
+        public async Task<HttpResponseMessage> SelectAll(string table)
+        {
+            string url = baseUrl + Database + "/";
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.GetAsync("collections/" + table + "?apiKey=" + ApiKey);
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> Write(string table, object data)
+        {
+            string url = baseUrl + Database + "/";
+
+            var dataString = new JavaScriptSerializer().Serialize(data);
+            var content = new StringContent(dataString, Encoding.Unicode, "application/json");
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+
+            return await client.PostAsync("collections/" + table + "?apiKey=" + ApiKey, content);
+        }
+    }
+}
