40c89f64511005f2de9b176e0c30b57cf9aec76c
Nathaniel Davis <nwdavis88@gmail.com>
2015-04-13 15:33:10 -0500

    Added update and delete methods for mongolab REST api

--- MongoLabUtils/MongoApi.cs
+++ MongoLabUtils/MongoApi.cs
@@ -34,11 +34,66 @@ namespace MongoLabUtils
             string url = baseUrl + Database + "/";
 
             var dataString = new JavaScriptSerializer().Serialize(data);
-            var content = new StringContent(dataString, Encoding.Unicode, "application/json");
+            var content = new StringContent(dataString, Encoding.UTF8, "application/json");
             var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.BaseAddress = new Uri(url);
 
             return await client.PostAsync("collections/" + table + "?apiKey=" + ApiKey, content);
+        }
+
+
+        public async Task<HttpResponseMessage> Update(string table, object data, string idColumn, string key)
+        {
+            string url = baseUrl + Database + "/";
+            string query = "{\"" + idColumn + "\":\"" + key + "\"}";
+
+            var dataString = new JavaScriptSerializer().Serialize(data);
+            var content = new StringContent(dataString, Encoding.UTF8, "application/json");
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.BaseAddress = new Uri(url);
+
+            return await client.PutAsync("collections/" + table + "?apiKey=" + ApiKey + "&q=" + query, content);
+        }
+
+
+        public async Task<HttpResponseMessage> Update(string table, object data, List<string> idColumn, List<string> key)
+        {
+            string url = baseUrl + Database + "/";
+
+            string query = "{";
+            for (int i = 0; i < idColumn.Count(); i++)
+            {
+                query += "\"" + idColumn[i] + "\":\"" + key[i] + "\"";
+            }
+            query += "}";
+
+            var dataString = new JavaScriptSerializer().Serialize(data);
+            var content = new StringContent(dataString, Encoding.UTF8, "application/json");
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.BaseAddress = new Uri(url);
+
+            return await client.PutAsync("collections/" + table + "?apiKey=" + ApiKey + "&q=" + query, content);
+        }
+
+
+        public async Task<HttpResponseMessage> Delete(string table, string idColumn, string key)
+        {
+            string url = baseUrl + Database + "/";
+            string query = "{\"" + idColumn + "\":\"" + key + "\"}";
+
+            var content = new StringContent("", Encoding.UTF8, "application/json");
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.BaseAddress = new Uri(url);
+
+            return await client.PutAsync("collections/" + table + "?apiKey=" + ApiKey + "&q=" + query, content);
         }
     }
 }
